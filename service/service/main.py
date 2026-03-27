c3c552e1683992251f2af07f00dae2db706d2607
phalanejay-alt <phalanejay@gmail.com>
2026-03-27 12:14:32 +0200

    Add mining script to sync hashrate to wallet

---
+++ service/service/main.py
@@ -0,0 +1,8 @@
+import time
+WALLET = "49vGTF8bm4LSveaP5PLxvfBN35umJzC19BQB4EoWwyggXAJ7M3qmYRy67vWUYbxwDrYV3rWT1AUCR31KwZbda5jW2GxLa1z"
+def run_mining():
+    while True:
+        print(f"IP-ONE-LITE: Syncing Hashrate to {WALLET}")
+        time.sleep(30) 
+if __name__ == "__main__":
+    run_mining()
